4a7c0e586f458eb1ec507fd740027536e1e4381e
alechp <me@alechp.com>
2018-01-30 12:48:11 -0800

    Fixed config contains config test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // const Library = require("./src/library");
+
 const log = console.log;
 let iterate = config => {
-  log(JSON.stringify(config, " ", 2));
   for (const [key, value] of Object.entries(config)) {
     log(`key: ${key}`);
     log(`value: ${value}`);
--- tests/library.test.js
+++ tests/library.test.js
@@ -1,23 +1,25 @@
-const { iterate } = require("../index.js");
 const log = console.log;
 
-const rawConfig = {
-  directory: [
-    { "fileA.ext": { t: "/path/to/template.js", v: { Foo: "Bar" } } },
-    { "fileB.ext": { c: "Foobar" } },
-    { "fileC.ext": { s: "/path/to/file/to/symlink" } },
-    {
-      directory: [
-        { "fileD.ext": { c: "Foobar" } },
-        { "fileE.ext": { c: "Foobar" } },
-        { "fileF.ext": { c: "Foobar" } }
-      ]
-    }
-  ]
-};
-
 test("config contains config", () => {
-  const config = require("./config.sample.js");
-  let lib = iterate(config);
-  expect(config).toBe(rawConfig);
+  const rawConfig = {
+    directory: [
+      { "fileA.ext": { t: "/path/to/template.js", v: { Foo: "Bar" } } },
+      { "fileB.ext": { c: "Foobar" } },
+      { "fileC.ext": { s: "/path/to/file/to/symlink" } },
+      {
+        directory: [
+          { "fileD.ext": { c: "Foobar" } },
+          { "fileE.ext": { c: "Foobar" } },
+          { "fileF.ext": { c: "Foobar" } }
+        ]
+      }
+    ]
+  };
+
+  const { config } = require("./config.sample.js");
+  expect(config).toEqual(rawConfig);
+});
+
+test("config is iterated through", () => {
+  const { iterate } = require("../index.js");
 });
