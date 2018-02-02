f9ae8d33100b122f25293ffc3525d33f54969a87
alechp <me@alechp.com>
2018-02-01 16:39:56 -0800

    Added brainstorm notes

--- src/library-reduce.js
+++ src/library-reduce.js
@@ -1,6 +1,20 @@
 const log = console.log;
 const chalk = require("chalk");
 
+/*
+===============================================
+Brainstorm
+===============================================
+1. Create placeholder directories object
+2. Grab all object keys in current object
+3. Filter for files (ie. contains *.*)
+4. Generate files
+5. Reduce files from original object 
+6. Return new object of directories
+7. Recurse with object of directories
+===============================================
+*/
+
 let Library = config => {
   config.reduce();
 };
