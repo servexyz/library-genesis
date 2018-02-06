7c577038003019afbab79602c16eeaa264df66fd
alechp <me@alechp.com>
2018-02-05 16:48:06 -0800

    Meh

--- src/library-reduce.js
+++ src/library-reduce.js
@@ -2,6 +2,7 @@ const log = console.log;
 const chalk = require("chalk");
 
 var Library = config => {
+  let hardstop = 1;
   return {
     //TODO: Create getType which returns "directory" or "file"
     printConfig: () => {
@@ -16,25 +17,50 @@ var Library = config => {
       );
       log(`${chalk.bold("Entries")}: \n ${chalk.green(entries)} \n `);
     },
+    has: function(obj, prop) {
+      return obj.hasOwnProperty(prop);
+    },
     parse: function(c = config) {
-      let propDir = c.hasOwnProperty("directory"); // => bool
-      let propChild = c.hasOwnProperty("children");
-      log(`Children: ${JSON.stringify(c.children)}`);
-      if (propDir || propChild) {
-        log(`configuration has directory property: ${propDir}`);
-        log(`configuration has children property: ${propChild}`);
-      }
-      for (let [k, v] of Object.entries(c)) {
-        log(`k:${k} v:${v}`);
+      if (hardstop < 10) {
+        log("hardstop ", hardstop);
+        log(`inside parse`);
+        hardstop++;
+        // let propDir = c.hasOwnProperty("directory"); // => bool
+        let propDir = this.has(c, "directory");
+        // let propChild = c.hasOwnProperty("children");
+        let propChild = this.has(c, "children");
+        // log(`Children: ${JSON.stringify(c.children)}`);
+        if (propDir) {
+          log(`propDir good`);
+          if (propChild) {
+            log(`propChild good`);
+            c.children
+              .map(n => {
+                if (this.has(n, "directory")) {
+                  //TODO: ensureDir()
+                  log(`directory: ${n.directory}`);
+                }
+                return n.children;
+              })
+              .map(child => {
+                log(`child: ${JSON.stringify(child, null, 2)}`);
+                return this.parse(child);
+              });
+          }
+        }
       }
     }
   };
 };
 /*
-      var newObject = Object.keys(myObject).reduce(function(previous, current) {
-        previous[current] = myObject[current] * myObject[current];
+      var newObject = Object.keys(c.children).reduce(function(
+        previous,
+        current
+      ) {
+        previous[current] = c.children[current];
         return previous;
-      }, {});
+      },
+      {});
     */
 module.exports = {
   Library
